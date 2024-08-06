6e1dafa7b4a290187584a8f98f6d8c8ba08d7d6e
harshitdhodi <154749538+harshitdhodi@users.noreply.github.com>
2024-08-06 14:48:17 +0530

    Delete route directory
    rsrdf

--- route/count.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const {getCounts , getLeadCount} = require("../controllers/dashboard")
-
-router.get("/allCount" , getCounts)
-router.get("/getLeadCount" , getLeadCount)
-
-module.exports = router;
