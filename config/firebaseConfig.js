252912861f785695043adca4fd0e97ed9a4a27fa
harshitdhodi <154749538+harshitdhodi@users.noreply.github.com>
2024-08-06 16:04:46 +0530

    Update firebaseConfig.js
    xfgdged

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -2,11 +2,12 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('../route/lead-management-system-245fb-firebase-adminsdk-vn38p-0f8837638f.json');
 
+
 const initializeFirebase = () => {
   if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
-      databaseURL: 'https://lead-management-system-245fb-default-rtdb.firebaseio.com/'
+      databaseURL: "https://lead-management-system-245fb-default-rtdb.firebaseio.com"
     });
   }
 };
