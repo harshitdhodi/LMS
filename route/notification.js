61aec3d62081d399b1c418d7505191e5b53eeae3
harshitdhodi <154749538+harshitdhodi@users.noreply.github.com>
2024-08-13 10:43:48 +0530

    Update notification.js
    dobne

--- route/notification.js
+++ route/notification.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const {getNotification} = require("../controllers/notification")
+const {getNotification , updateIsExcept} = require("../controllers/notification")
 const { authMiddleware } = require("../middleware/adminOruser");
 router.get("/getNotification" ,authMiddleware , getNotification )
-
+router.put("/updateIsExcept" ,authMiddleware , updateIsExcept )
 module.exports = router
