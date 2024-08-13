c2fd8bffcb218d6e40ed44b0a48b9fc08d497871
harshitdhodi <154749538+harshitdhodi@users.noreply.github.com>
2024-08-13 10:59:37 +0530

    Update notification.js
    done

--- controllers/notification.js
+++ controllers/notification.js
@@ -37,42 +37,39 @@ const getNotification = asyncHandler(async (req, res) => {
     }
   });
   
-  const updateIsExcept = async (req, res) => {
+   const updateIsExcept = async (req, res) => {
     try {
-      const { id } = req.query; // User ID from query parameter
+      const { id } = req.query; // Notification ID from query parameter
   
-      // Find the notification by user ID
-      const notifications = await Notification.find(id);
-      if (!notifications.length) {
-        return res.status(404).json({ msg: "No notifications found for the user." });
+      // Find the notification by ID
+      const notification = await Notification.findById(id);
+      if (!notification) {
+        return res.status(404).json({ msg: "Notification not found." });
       }
   
-      // Process each notification
-      for (const notification of notifications) {
-        // Update isExcept status
-        notification.isExcept = true; // Set to true if that is your requirement
-        await notification.save();
+      // Update isExcept status
+      notification.isExcept = true; // Set to true if that is your requirement
+      await notification.save();
   
-        // If isExcept is true, save data to Leads schema
-        if (notification.isExcept) {
-          const newLead = new Leads({
-            firstname: notification.firstname,
-            lastname: notification.lastname,
-            email: notification.email,
-            mobile: notification.mobile,
-            companyname: notification.companyname,
-            user: notification.user,
-            leadInfo: notification.leadInfo,
-            leadsDetails: notification.leadsDetails,
-            byLead: notification.byLead,
-            leadType: notification.leadType,
-          });
+      // If isExcept is true, save data to Leads schema
+      if (notification.isExcept) {
+        const newLead = new Leads({
+          firstname: notification.firstname,
+          lastname: notification.lastname,
+          email: notification.email,
+          mobile: notification.mobile,
+          companyname: notification.companyname,
+          user: notification.user,
+          leadInfo: notification.leadInfo,
+          leadsDetails: notification.leadsDetails,
+          byLead: notification.byLead,
+          leadType: notification.leadType,
+        });
   
-          await newLead.save();
+        await newLead.save();
   
-          // Optionally, you can delete the notification entry if it's no longer needed
-          // await Notification.findByIdAndDelete(notification._id);
-        }
+        // Optionally, delete the notification entry if it's no longer needed
+        // await Notification.findByIdAndDelete(id);
       }
   
       res.status(200).json({ msg: "Status updated and data moved to Leads schema." });
