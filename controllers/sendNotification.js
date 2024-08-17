3c649fe642f250afb2f5ad7ea025a822c573f292
harshitdhodi <154749538+harshitdhodi@users.noreply.github.com>
2024-08-17 11:16:49 +0530

    Update sendNotification.js
    done

--- controllers/sendNotification.js
+++ controllers/sendNotification.js
@@ -7,20 +7,25 @@ initializeFirebase();
 
 const sendNotification = async (notificationData) => {
   try {
-    // Send notification using Firebase Admin SDK
-    const response = await admin.messaging().sendMulticast({
-      tokens: notificationData.tokens,
+    if (!notificationData.token) {
+      throw new Error('Notification token is required');
+    }
+
+    const response = await admin.messaging().send({
+      token: notificationData.token,  // Single device token
       notification: {
         title: notificationData.title,
         body: notificationData.body,
       },
     });
-    
+  
     return response; // Return the response from Firebase
   } catch (error) {
     console.error('Error sending notification:', error);
     throw error; // Rethrow the error to be handled by the calling function
   }
 };
+
+
 
 module.exports = { sendNotification };
