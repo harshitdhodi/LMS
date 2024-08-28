dcb1a81b7989dfd4ef641a4cab187358eb02d035
harshitdhodi <154749538+harshitdhodi@users.noreply.github.com>
2024-08-28 08:26:02 +0530

    Update notification.js

--- models/notification.js
+++ models/notification.js
@@ -1,59 +1,80 @@
-const mongoose = require('mongoose'); // Erase if already required
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const User = require("../models/user")
-// Declare the Schema of the Mongo model
-const  NotificationleadSchema = new mongoose.Schema({
-    firstname:{
-        type:String,
-        required:true, 
-    },
-    lastname:{
-        type:String,
-        required:true, 
-    },
-    email:{
-        type:String,
-        required:true,
-      
-    },
-    mobile:{
-        type:String,
-        required:true,
-      
-    },
-    status: {
-        type: String,
-        default:"New"
-      },
-    byLead: {
-        type: String,
-        default:"admin"
-    },
-    leadType:{
-        type:String
-    },
-    companyname:{   
-        type:String,
-        required:true,
-    },
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: User,
-        required: true,
-      },
-    leadInfo:{
-        type:String,
-        required:true,
-    },
-    leadsDetails :{
-        type:String,
-        required:true,
-    },
-    isExcept:{
-        type:Boolean,
-        default:null
-    }
+
+const notificationSchema = new Schema({
+  firstname: {
+    type: String,
+    default: ""
+  },
+  lastname: {
+    type: String,
+    default: ""
+  },
+  email: {
+    type: String,
+    default: ""
+  },
+  mobile: {
+    type: String,
+    default: ""
+  },
+  status: {
+    type: String,
+    default: "New"
+  },
+  byLead: {
+    type: String,
+    default: "admin"
+  },
+  leadType: {
+    type: String,
+    default: ""
+  },
+  companyname: {
+    type: String,
+    default: ""
+  },
+  leadInfo: {
+    type: String,
+    default: ""
+  },
+  leadsDetails: {
+    type: String,
+    default: ""
+  },
+  isExcept: {
+    type: Boolean,
+    default: null
+  },
+  name: {
+    type: String,
+    default: ""
+  },
+  subject: {
+    type: String,
+    default: ""
+  },
+  phone: {
+    type: String,
+    default: ""
+  },
+  message: {
+    type: String,
+    default: ""
+  },
+  API_KEY: {
+    type: String,
+
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  } 
+}, {
+  timestamps: true
 });
 
-//Export the model
-module.exports = mongoose.model('Notification', NotificationleadSchema);
+const Notification = mongoose.model('Notification', notificationSchema);
+
+module.exports = Notification;
